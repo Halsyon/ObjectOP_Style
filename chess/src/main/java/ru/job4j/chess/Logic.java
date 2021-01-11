2a71475fd39a9916ada447202ad9aac4259babea
SlartiBartFast-art <JekHamond@tutanota.com>
2021-01-11 22:23:24 +0500

    2. Каркас шахматной доски [#392264]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -2,16 +2,18 @@ package ru.job4j.chess;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
-
 import java.util.Arrays;
 
 public final class Logic {
     private final Figure[] figures = new Figure[32]; // массив на 32 элемента
     private int index = 0;
 
+/** Метод добавляет фигуру в массив фигур
+ * @param figure новая фигура *
+ * */
     public void add(Figure figure) {
         figures[index++] = figure;
-    } // на входе фигура, ее место в массиве
+    }
 
     public void move(Cell source, Cell dest) //
             throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
@@ -20,37 +22,35 @@ public final class Logic {
         if (free(steps)) {
             figures[index] = figures[index].copy(dest);
         }
-
     }
 
     /**
      * Метод free должен пройтись по массиву figures и проверить,
-     * что фигуры из не занимают элементы из массива cells.
+     * что фигуры из него не занимают элементы из массива cells.
      */
-
-    private boolean free(Cell[] steps) throws OccupiedCellException { //
-        for (Cell celX : steps) {
-            System.out.println("координаты: " + celX.getX() + " " + celX.getY());
-            Cell temp = Cell.findBy(celX.getX(), celX.getY());
-            if (temp != null) {
-                return false;
-            }
+    private boolean free(Cell[] steps) throws OccupiedCellException {
+        for (int i = 0; i < figures.length; i++) {
+                for (int j = 0; j < steps.length; j++) {
+                    if (figures[i] != null && figures[i].position().equals(steps[j])) {
+                        throw new OccupiedCellException();
+                    }
+                }
         }
         return true;
     }
 
     public void clean() {
-        Arrays.fill(figures, null);
-        index = 0;
+            Arrays.fill(figures, null);
+            index = 0;
     }
 
     private int findBy(Cell cell) throws FigureNotFoundException {
-        for (int index = 0; index != figures.length; index++) {
-            Figure figure = figures[index];
-            if (figure != null && figure.position().equals(cell)) {
-                return index;
+            for (int index = 0; index != figures.length; index++) {
+                Figure figure = figures[index];
+                if (figure != null && figure.position().equals(cell)) {
+                    return index;
+                }
             }
+            throw new FigureNotFoundException();
         }
-        throw new FigureNotFoundException();
     }
-}
--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -11,18 +11,18 @@ public class BishopBlack implements Figure {
     }
 
     /**
-     * Это метод используется для получения позиции фигуры на поле. Он возвращает тип перечисления.
+     * Метод используется для получения позиции фигуры на поле.
+     * @return  возвращает тип перечисления.
      */
-
     @Override
     public Cell position() {
         return position;
     }
 
     /**
-     * Этот метод возвращает массив с клетками, которые должна пройти фигура от клетки position до клетки dest.
+     * Метод возвращает массив с клетками,
+     * которые должна пройти фигура от клетки position до клетки dest.
      */
-
     @Override
     public Cell[] way(Cell dest) {
         if (!isDiagonal(position, dest)) { //Пользователь может перетащить слона на клетку, которая отличается от диагонали.
@@ -31,20 +31,14 @@ public class BishopBlack implements Figure {
         }
         int size = Math.abs(position().getX() - dest.getX()); //как определить размер?
         Cell[] steps = new Cell[size];
-        int deltaX = position.getX();
-        int deltaY = position.getY();
+        int deltaX = dest.getX() - position().getX() > 0 ? 1 : -1;
+        int deltaY = dest.getY() - position().getY() > 0 ? 1 : -1;
+        int x = position.getX();
+        int y = position.getY();
         for (int index = 0; index < size; index++) {
-            if (position.getX() < dest.getX()) {
-                deltaX++;
-            } else {
-                deltaX--;
-            }
-            if (position.getY() < dest.getY()) {
-                deltaY++;
-            } else {
-                deltaY--;
-            }
-            steps[index] = Cell.findBy(deltaX, deltaY);
+                x += deltaX;
+                y += deltaY;
+            steps[index] = Cell.findBy(x, y);
         }
         return steps;
     }
@@ -52,18 +46,13 @@ public class BishopBlack implements Figure {
     /**
      * source где стоит фигура/ dest -куда ее хотят поставить
      */
-
     public boolean isDiagonal(Cell source, Cell dest) {
-        if (Math.abs(source.getX() - dest.getX()) == Math.abs(source.getY() - dest.getY())) {
-            return true;
-        }
-        return false;
+        return Math.abs(source.getX() - dest.getX()) == Math.abs(source.getY() - dest.getY());
     }
 
     /**
      * Этот метод создаст объект класса с позицией dest.
      */
-
     @Override
     public Figure copy(Cell dest) {
         return new BishopBlack(dest);
